a1cb3d3d3170cb32ded103ebd6dbdb51674641e4
Guy Bianco <gbianco@redhat.com>
2014-01-20 16:13:20 -0500

    added service interface

---
+++ src/main/java/org/jboss/as/elock/webservice/ElockService.java
@@ -0,0 +1,43 @@
+package org.jboss.as.elock.webservice;
+
+/**
+ * 
+ * @author Guy J. Bianco IV
+ *
+ */
+public interface ElockService {
+	
+	/**
+	 * Checks whether or not the specified user is allowed to open the specified door.
+	 * 
+	 * @param userId
+	 * @param doorId
+	 * @return
+	 */
+	public boolean isValidUnlock(int userId, int doorId);
+	
+	/**
+	 * Get the full permissions available to the specified user.
+	 * 
+	 * @param userId
+	 * @return
+	 */
+	public String getPerms(int userId);
+	
+	/**
+	 * Get the required permission to unlock the specified door.
+	 * 
+	 * @param doorId
+	 * @return
+	 */
+	public int getReqPerms(int doorId);
+	
+	/**
+	 * Unlock the specified door, if the specified user has the permission to do so.
+	 * 
+	 * @param userId
+	 * @param doorId
+	 * @return
+	 */
+	public boolean unlock(int userId, int doorId);
+}
