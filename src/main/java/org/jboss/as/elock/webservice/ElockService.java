12858070917a43d0161508e9137c87fb4d860565
Guy Bianco <gbianco@redhat.com>
2014-01-21 09:32:50 -0500

    added registration methods to service interface

--- src/main/java/org/jboss/as/elock/webservice/ElockService.java
+++ src/main/java/org/jboss/as/elock/webservice/ElockService.java
@@ -1,7 +1,9 @@
 package org.jboss.as.elock.webservice;
 
+import java.util.Date;
+
 /**
- * CDI service which responds with whether or not a user has permission to unlock a door.
+ * CDI service which deals with whether or not a user has permission to unlock a door.
  * 
  * @author Guy J. Bianco IV
  *
@@ -9,27 +11,22 @@ package org.jboss.as.elock.webservice;
 public interface ElockService {
 	
 	/**
-	 * Checks whether or not the specified user is allowed to open the specified door.
-	 * 
 	 * @param userId
 	 * @param doorId
-	 * @return
+	 * 
+	 * @return Whether or not the specified user is allowed to open the specified door.
 	 */
 	public boolean isValidUnlock(int userId, int doorId);
 	
 	/**
-	 * Get the full permissions available to the specified user.
-	 * 
 	 * @param userId
-	 * @return
+	 * @return The highest permission available to the specified user (across all cards).
 	 */
 	public String getPerms(int userId);
 	
 	/**
-	 * Get the required permission to unlock the specified door.
-	 * 
 	 * @param doorId
-	 * @return
+	 * @return Required permission to unlock the specified door.
 	 */
 	public int getReqPerms(int doorId);
 	
@@ -38,7 +35,43 @@ public interface ElockService {
 	 * 
 	 * @param userId
 	 * @param doorId
-	 * @return
+	 * 
+	 * @return Whether or not the request succeeded.
 	 */
 	public boolean unlock(int userId, int doorId);
+	
+	/**
+	 * @param name <em>Exact</em> name of a registered user.
+	 * @return The ID of specified user, or -1 if no match is found.
+	 */
+	public long findUserId(String name);
+	
+	/**
+	 * Register a new user in the system.
+	 * 
+	 * @param name Full name of the user.
+	 * @param birthdate - Date object representing the user's DOB.
+	 * 
+	 * @return Whether or not the registration succeeded.
+	 */
+	public boolean registerUser(String name, Date birthdate);
+	
+	/**
+	 * Register a new door in the system.
+	 * 
+	 * @param reqPerm Required permission level to unlock the door.
+	 * @return Whether or not the registration succeeded.
+	 */
+	public boolean registerDoor(int reqPerm);
+	
+	/**
+	 * Register a new card (to a user) in the system.
+	 * 
+	 * @param userId The ID of the user to own this card (use in conjunction with {@link #findUserId(String name)}.
+	 * @param perm Highest permission the card is allowed to access.
+	 * @param expiration Date object representing the expiration date of the card.
+	 * 
+	 * @return Whether or not the registration succeeded.
+	 */
+	public boolean registerCard(long userId, int perm, Date expiration);
 }
