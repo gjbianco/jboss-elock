ff3544bfdc3caf0cc3112c6ac2569e6a757bf360
Guy Bianco <gbianco@redhat.com>
2014-01-22 12:30:03 -0500

    changed to container managed DAO

--- src/main/java/org/jboss/as/elock/persistence/dao/AbstractDao.java
+++ src/main/java/org/jboss/as/elock/persistence/dao/AbstractDao.java
@@ -1,17 +1,10 @@
 package org.jboss.as.elock.persistence.dao;
 
-import javax.annotation.Resource;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
-import javax.transaction.UserTransaction;
+import javax.persistence.PersistenceContext;
 
 public abstract class AbstractDao {
 	
-	@PersistenceUnit(unitName = "primary")
-	protected EntityManagerFactory entityManagerFactory;
-	protected EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-	@Resource
-	UserTransaction utx;
+	@PersistenceContext(unitName = "primary")
+	protected EntityManager entityManager;
 }
