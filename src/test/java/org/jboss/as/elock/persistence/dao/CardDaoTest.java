0f27ea4536f6c1ff6f9e8b0e835c0b306de2e250
Guy Bianco <gbianco@redhat.com>
2014-01-24 15:44:50 -0500

    added unit test files for DAO's

---
+++ src/test/java/org/jboss/as/elock/persistence/dao/CardDaoTest.java
@@ -0,0 +1,44 @@
+package org.jboss.as.elock.persistence.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CardDaoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/org/jboss/as/elock/persistence/dao/UserDaoTest.java
@@ -0,0 +1,65 @@
+package org.jboss.as.elock.persistence.dao;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import junit.framework.Assert;
+
+import org.jboss.as.elock.model.Card;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserDaoTest {
+	
+	@PersistenceContext(unitName = "userTest")
+	EntityManager testEntityManager;
+	
+	EntityManagerFactory factory;
+
+	@Before
+	public void setUp() throws Exception {
+		factory = Persistence.createEntityManagerFactory("userTest");
+		testEntityManager = factory.createEntityManager();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreate() {
+		//Query query = testEntityManager.createNamedQuery(name)
+	}
+
+	@Test
+	public void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAll() {
+		Query cardQuery = testEntityManager.createNamedQuery("findAll");
+		List<Card> cardList = cardQuery.getResultList();
+		List<Card> list = testEntityManager.createQuery("FROM User").getResultList();
+		assertEquals(cardList.size(), list.size());
+	}
+
+}
